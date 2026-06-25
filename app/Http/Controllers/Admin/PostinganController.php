1b22c95c1097cccb86bc3beae7a0695247e6a3a1
ekaputra012309 <eko012309@gmail.com>
2026-06-25 14:55:47 +0700

    fix posting on hosting

--- app/Http/Controllers/Admin/PostinganController.php
+++ app/Http/Controllers/Admin/PostinganController.php
@@ -8,8 +8,6 @@ use App\Models\Admin\Postingan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use DataTables;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 
 class PostinganController extends Controller
 {
@@ -39,38 +37,25 @@ class PostinganController extends Controller
     // upload
     public function upload(Request $request)
     {
-        if ($request->hasFile('upload')) {
-            $file = $request->file('upload');
+        if($request->hasFile('upload')) {
+            $extension = $request->file('upload')->getClientOriginalExtension();
+            $filename = 'post_'.time().'.'.$extension;
 
-            $filename = 'post_' . time() . '_' . Str::random(8) . '.' . $file->getClientOriginalExtension();
-
-            // store in storage/app/public/files/gambar_postingan
-            $path = $file->storeAs('files/gambar_postingan', $filename, 'public');
-
-            $url = asset('storage/' . $path);
-
-            return response()->json([
-                'filename' => $filename,
-                'uploaded' => 1,
-                'url' => $url
-            ]);
+            $request->file('upload')->move(upload_path('files/gambar_postingan'), $filename);
+ 
+            $url = asset('files/gambar_postingan/'.$filename);
+            return response()->json(['filename' => $filename, 'uploaded' => 1, 'url' => $url]);
         }
-
-        return response()->json([
-            'uploaded' => 0,
-            'error' => ['message' => 'No file uploaded']
-        ], 400);
     }
 
     public function store(Request $request)
     {
         // get gambar
-        if ($request->hasFile('gambar_postingan')) {
+        if($request->hasFile('gambar_postingan')) {
             $file = $request->file('gambar_postingan');
-            $file_name = 'post_banner_' . time() . '_' . Str::random(8) . '.' . $file->getClientOriginalExtension();
-
-            // save to storage/app/public/files/gambar_postingan/banner
-            $file->storeAs('files/gambar_postingan/banner', $file_name, 'public');
+            $extension = $file->getClientOriginalExtension();
+            $file_name = 'post_banner_'.time().'.'.$extension;
+            $file->move(upload_path('files/gambar_postingan/banner'), $file_name);
         } else {
             $file_name = null;
         }
@@ -102,23 +87,31 @@ class PostinganController extends Controller
     public function update(Request $request)
     {
         $id = $request->id;
-        // get data by id
-        $data = Postingan::getById($id);
-        // get gambar
+        $data = Postingan::find($id); // FIXED
+    
+        $file_name = $data->gambar;
+    
         if ($request->hasFile('gambar_postingan')) {
-            // delete old file if exists
-            if ($data->gambar && Storage::disk('public')->exists('files/gambar_postingan/banner/' . $data->gambar)) {
-                Storage::disk('public')->delete('files/gambar_postingan/banner/' . $data->gambar);
+    
+            $uploadPath = upload_path('files/gambar_postingan/banner/');
+    
+            // delete old image safely
+            if (!empty($data->gambar)) {
+                $oldFile = $uploadPath . $data->gambar;
+    
+                if (file_exists($oldFile) && is_file($oldFile)) {
+                    @unlink($oldFile);
+                }
             }
-
+    
+            // upload new image
             $file = $request->file('gambar_postingan');
-            $file_name = 'post_banner_' . time() . '_' . Str::random(8) . '.' . $file->getClientOriginalExtension();
-
-            $file->storeAs('files/gambar_postingan/banner', $file_name, 'public');
-        } else {
-            $file_name = $data->gambar;
+            $extension = $file->getClientOriginalExtension();
+            $file_name = 'post_banner_' . time() . '.' . $extension;
+    
+            $file->move($uploadPath, $file_name);
         }
-
+    
         $data->judul = $request->judul;
         $data->slug = $request->slug;
         $data->konten = $request->konten;
@@ -126,20 +119,30 @@ class PostinganController extends Controller
         $data->status = 'Draft';
         $data->updated_by = Auth::user()->id;
         $data->save();
-
+    
         return redirect()->route('postingan')->with('success', 'Data berhasil diubah');
     }
 
     // delete
     public function delete(Request $request)
     {
-        $id = $request->id;
-        $data = Postingan::find($id);
-        if ($data->gambar && Storage::disk('public')->exists('files/gambar_postingan/banner/' . $data->gambar)) {
-            Storage::disk('public')->delete('files/gambar_postingan/banner/' . $data->gambar);
+        $data = Postingan::find($request->id);
+    
+        if ($data) {
+    
+            $uploadPath = upload_path('files/gambar_postingan/banner/');
+    
+            if (!empty($data->gambar)) {
+                $file = $uploadPath . $data->gambar;
+    
+                if (file_exists($file) && is_file($file)) {
+                    @unlink($file);
+                }
+            }
+    
+            $data->delete();
         }
-        $data->delete();
-
+    
         return response()->json([
             'success' => true,
             'message' => 'Berita berhasil dihapus!'
