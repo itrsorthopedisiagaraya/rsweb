7b9f279921e8a623a186ef74aee083232c2b20f3
ekaputra012309 <eko012309@gmail.com>
2026-06-26 17:07:23 +0700

    add image under development

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,51 +2,23 @@
 @section('title', 'Dashboard')
 
 @section('breadcrumb')
-{{ Breadcrumbs::render('dashboard') }}
+    {{ Breadcrumbs::render('dashboard') }}
 @endsection
 
 @section('content')
-<style>
-    .item-dashboard {
-    border: none;
-    border-radius: 10px;
-    border: .5px solid #a2a2a2;
-    padding: 20px;
-    margin: 20px;
-  }
-</style>
-<div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <div class="card item-dashboard">
-          <h3>Status Pelayanan Pasien</h3>
-          <p>Jumlah pasien yang dirawat: 75</p>
-          <p>Jumlah pasien yang dipulangkan: 20</p>
+    <style>
+        .item-dashboard {
+            border: none;
+            border-radius: 10px;
+            border: .5px solid #a2a2a2;
+            padding: 20px;
+            margin: 20px;
+        }
+    </style>
+    <div class="container">
+        <div class="row">
+            <img src="{{ asset('assets/images/under development.png') }}" alt="Dashboard" class="img-fluid mx-auto d-block"
+                style="max-width: 50%;">
         </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card item-dashboard">
-          <h3>Pemesanan Janji Dokter</h3>
-          <p>Jumlah stok obat yang tersedia: 500</p>
-          <p>Obat yang mendekati kadaluarsa: 10</p>
-        </div>
-      </div>
     </div>
-    <div class="row">
-      <div class="col-md-6">
-        <div class="card item-dashboard">
-          <h3>Statistik Obat dan Persediaan</h3>
-          <p>Jumlah stok obat yang tersedia: 500</p>
-          <p>Obat yang mendekati kadaluarsa: 10</p>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card item-dashboard">
-          <h3>Kamar Rawat Inap</h3>
-          <p>Kamar terisi: 30</p>
-          <p>Kamar kosong: 10</p>
-        </div>
-      </div>
-    </div>
-</div>
 @endsection
