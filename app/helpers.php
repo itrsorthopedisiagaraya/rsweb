4866157fb4a7b4d13b569b689514b60dcc36b0ec
ekaputra012309 <eko012309@gmail.com>
2026-07-15 11:04:55 +0700

    add local and hosting public path

--- app/helpers.php
+++ app/helpers.php
@@ -5,9 +5,15 @@ use App\Models\PartnerAsuransi;
 if (!function_exists('upload_path')) {
     function upload_path($dir)
     {
+        // hosting path 
         $path = file_exists(base_path('../public_html'))
             ? base_path('../public_html/' . ltrim($dir, '/'))
             : public_path($dir);
+        
+        // local path
+        // $path = file_exists(base_path('../rsweb2'))
+        //     ? base_path('../rsweb2/' . ltrim($dir, '/'))
+        //     : public_path($dir);
 
         if (!is_dir($path)) {
             mkdir($path, 0755, true);
